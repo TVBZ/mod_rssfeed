76e87d24e0030fd271974dbc7d632e793f8b225a
TVBZ <43469713+TVBZ@users.noreply.github.com>
2020-06-21 21:45:58 +0200

    add more channel info

--- tmpl/default.php
+++ tmpl/default.php
@@ -20,9 +20,17 @@ $document->addStyleSheet(Uri::root(true)."/modules/mod_rssfeed/assets/css/styles
 
 ?>
 
+
 <?php if ($params["channel"] == "1") : ?>
     <h1><?php echo $rss->channel->title; ?></h1>
 <?php endif; ?>
+<p>Source: <?php echo $rss->channel->link; ?></p>
+<p>Logo: <img src="<?php echo $rss->channel->image->url; ?>" /></p>
+<p>Copyright: <?php echo $rss->channel->copyright; ?></p>
+<p>Managing editor: <?php echo $rss->channel->managingEditor; ?></p>
+<p>Webmaster: <?php echo $rss->channel->webMaster; ?></p>
+<p>Last build: <?php echo $rss->channel->lastBuildDate; ?></p>
+<p>Description: <?php echo $rss->channel->description; ?></p>
 
 <div>
 <?php foreach ($rss->channel->item as $item) : ?>
